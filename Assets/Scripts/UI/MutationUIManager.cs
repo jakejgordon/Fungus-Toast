1d650ada38180c451326775157a6d3a305e7f75a
Jake Gordon <jakejgordon@gmail.com>
2025-04-25 20:34:20 -0400

    added LeftSidePanel and Spend Points button

---
+++ Assets/Scripts/UI/MutationUIManager.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using FungusToast.Game;
+
+namespace FungusToast.UI
+{
+    public class MutationUIManager : MonoBehaviour
+    {
+        [Header("References")]
+        public MutationManager mutationManager;
+        public GameObject mutationTreePanel;
+        public Button spendPointsButton;
+        public TextMeshProUGUI spendPointsButtonText;
+
+        [Header("Animation")]
+        public float slideDuration = 0.5f;
+        private bool isTreeOpen = false;
+
+        private RectTransform mutationTreeRect;
+
+        private void Start()
+        {
+            mutationTreeRect = mutationTreePanel.GetComponent<RectTransform>();
+            CloseTreeInstant();
+            UpdateSpendPointsButton();
+        }
+
+        private void Update()
+        {
+            UpdateSpendPointsButton();
+        }
+
+        private void UpdateSpendPointsButton()
+        {
+            if (mutationManager.CurrentMutationPoints > 0)
+            {
+                spendPointsButton.interactable = true;
+                spendPointsButtonText.text = $"Spend {mutationManager.CurrentMutationPoints} Points!";
+                // Optionally pulse/glow the button here (later)
+            }
+            else
+            {
+                spendPointsButton.interactable = false;
+                spendPointsButtonText.text = "No Points Available";
+            }
+        }
+
+        public void OnSpendPointsClicked()
+        {
+            if (!isTreeOpen)
+                OpenTree();
+            else
+                CloseTree();
+        }
+
+        private void OpenTree()
+        {
+            mutationTreePanel.SetActive(true);
+            isTreeOpen = true;
+            // (Optional) Animate sliding open here later
+        }
+
+        private void CloseTree()
+        {
+            mutationTreePanel.SetActive(false);
+            isTreeOpen = false;
+            // (Optional) Animate sliding closed here later
+        }
+
+        private void CloseTreeInstant()
+        {
+            mutationTreePanel.SetActive(false);
+            isTreeOpen = false;
+        }
+    }
+}
