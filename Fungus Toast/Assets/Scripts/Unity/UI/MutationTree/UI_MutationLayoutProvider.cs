e2555dc971cc45f26acf8b311698def481e20a56
Jake Gordon <jakejgordon@gmail.com>
2025-05-14 12:19:11 -0400

    fixed compile issue and some scene references

--- Fungus Toast/Assets/Scripts/Unity/UI/MutationTree/UI_MutationLayoutProvider.cs
+++ Fungus Toast/Assets/Scripts/Unity/UI/MutationTree/UI_MutationLayoutProvider.cs
@@ -13,26 +13,26 @@ namespace FungusToast.Unity.UI.MutationTree
             new Dictionary<int, MutationLayoutMetadata>
             {
                 /* ---------------- Growth (col 0) -------------------- */
-                { MutationManager.MutationIds.MycelialBloom,       new MutationLayoutMetadata(0, 0, MutationCategory.Growth) },
-                { MutationManager.MutationIds.TendrilNorthwest,    new MutationLayoutMetadata(0, 1, MutationCategory.Growth) },
-                { MutationManager.MutationIds.TendrilNortheast,    new MutationLayoutMetadata(0, 2, MutationCategory.Growth) },
-                { MutationManager.MutationIds.TendrilSoutheast,    new MutationLayoutMetadata(0, 3, MutationCategory.Growth) },
-                { MutationManager.MutationIds.TendrilSouthwest,    new MutationLayoutMetadata(0, 4, MutationCategory.Growth) },
-                { MutationManager.MutationIds.MycotropicInduction, new MutationLayoutMetadata(0, 5, MutationCategory.Growth) },
+                { MutationIds.MycelialBloom,       new MutationLayoutMetadata(0, 0, MutationCategory.Growth) },
+                { MutationIds.TendrilNorthwest,    new MutationLayoutMetadata(0, 1, MutationCategory.Growth) },
+                { MutationIds.TendrilNortheast,    new MutationLayoutMetadata(0, 2, MutationCategory.Growth) },
+                { MutationIds.TendrilSoutheast,    new MutationLayoutMetadata(0, 3, MutationCategory.Growth) },
+                { MutationIds.TendrilSouthwest,    new MutationLayoutMetadata(0, 4, MutationCategory.Growth) },
+                { MutationIds.MycotropicInduction, new MutationLayoutMetadata(0, 5, MutationCategory.Growth) },
 
                 /* ------------ Cellular Resilience (col 1) ----------- */
-                { MutationManager.MutationIds.HomeostaticHarmony,       new MutationLayoutMetadata(1, 0, MutationCategory.CellularResilience) },
-                { MutationManager.MutationIds.ChronoresilientCytoplasm, new MutationLayoutMetadata(1, 1, MutationCategory.CellularResilience) },
-                { MutationManager.MutationIds.Necrosporulation,         new MutationLayoutMetadata(1, 2, MutationCategory.CellularResilience) },
+                { MutationIds.HomeostaticHarmony,       new MutationLayoutMetadata(1, 0, MutationCategory.CellularResilience) },
+                { MutationIds.ChronoresilientCytoplasm, new MutationLayoutMetadata(1, 1, MutationCategory.CellularResilience) },
+                { MutationIds.Necrosporulation,         new MutationLayoutMetadata(1, 2, MutationCategory.CellularResilience) },
 
                 /* ---------------- Fungicide (col 2) ----------------- */
-                { MutationManager.MutationIds.SilentBlight,         new MutationLayoutMetadata(2, 0, MutationCategory.Fungicide) },
-                { MutationManager.MutationIds.EncystedSpores,       new MutationLayoutMetadata(2, 1, MutationCategory.Fungicide) },
-                { MutationManager.MutationIds.PutrefactiveMycotoxin,new MutationLayoutMetadata(2, 2, MutationCategory.Fungicide) }, // NEW
+                { MutationIds.SilentBlight,         new MutationLayoutMetadata(2, 0, MutationCategory.Fungicide) },
+                { MutationIds.EncystedSpores,       new MutationLayoutMetadata(2, 1, MutationCategory.Fungicide) },
+                { MutationIds.PutrefactiveMycotoxin,new MutationLayoutMetadata(2, 2, MutationCategory.Fungicide) }, // NEW
 
                 /* --------------- Genetic Drift (col 3) -------------- */
-                { MutationManager.MutationIds.AdaptiveExpression, new MutationLayoutMetadata(3, 0, MutationCategory.GeneticDrift) },
-                { MutationManager.MutationIds.MutatorPhenotype,   new MutationLayoutMetadata(3, 1, MutationCategory.GeneticDrift) },
+                { MutationIds.AdaptiveExpression, new MutationLayoutMetadata(3, 0, MutationCategory.GeneticDrift) },
+                { MutationIds.MutatorPhenotype,   new MutationLayoutMetadata(3, 1, MutationCategory.GeneticDrift) },
             };
     }
 }
